14f047331461540956bbed3c2cab9882fb140982
paulacasas <desdesa78@gmail.com>
2025-11-10 12:29:11 -0500

    Operadores

---
+++ Basico/03-Operadores.js
@@ -0,0 +1,26 @@
+// Operadores aritméticos 
+console.log(2 + 3); // Suma
+console.log(10 - 2); // Resta
+console.log(4 * 2); // Multiplicación
+console.log(20 / 4); // División
+console.log(10 % 3); // Módulo (resto de la división)
+console.log(2 ** 2); // Exponente (elevado a la 2)
+
+// Operadores de comparación
+console.log(5 > 3); // Mayor que
+console.log(2 < 4); // Menor que
+console.log(5 >= 5); // Mayor o igual que
+console.log(3 <= 2); // Menor o igual que
+console.log(4 == "4"); // Igualdad (valor)
+console.log(4 === "4"); // Igualdad estricta (tipo y valor)
+console.log(5 != 8); // Diferente (valor)
+
+// Operadores lógicos
+console.log(true && false); // AND
+console.log(true || false); // OR
+console.log(!true); // NOT
+
+// Operadores ternarios
+const isRaining = true;
+// condicion (if)? valor Verdadero (else): valor Falso
+isRaining ? console.log("Lleva paraguas") : console.log("No lleva paraguas");
